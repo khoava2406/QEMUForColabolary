c5294717c50625ca431935cc6ba613318b832986
Matthew A Rogers <102123822+khoava2406@users.noreply.github.com>
2022-05-12 20:17:49 +0700

    Update Windows 8.1x64.sh

--- Windows 8.1x64.sh
+++ Windows 8.1x64.sh
@@ -10,7 +10,7 @@ sudo apt install qemu-system-x86 -y
 qemu-img create -f raw windows864.img 32G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
 wget -O windows864.iso 'https://dl.malwarewatch.org/windows/Windows%208.1%20%28x64%29.iso'
-clear
+echo "Key Windows 8.1 : XKY4K-2NRWR-8F6P2-448RF-CRYQH"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
   -m 11G \
