314ced7b34e93fddd4a8fa426e28e81f0efbfa89
H-Ra252 <101177330+H-Ra252@users.noreply.github.com>
2022-05-13 16:04:51 +0800

    Update Win7.sh

--- Win7.sh
+++ Win7.sh
@@ -1,7 +1,8 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 rm -rf ngrok-stable-linux-amd64.zip
-./ngrok authtoken 28NnMQVnOmczLoYJwFMTBoF7WQN_29asx8AbUXQveiriHuATN 
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP  
 nohup ./ngrok tcp --region ap 5900 &>/dev/null &
 sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
@@ -13,7 +14,7 @@ wget -O windows7.iso 'https://dl.malwarewatch.org/windows/Windows%207.iso'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
-  -m 8G \
+  -m 8192M \
   -cpu EPYC \
   -boot order=d \
   -drive file=windows7.iso,media=cdrom \
@@ -22,4 +23,4 @@ sudo qemu-system-x86_64 \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
-  -smp cores=8 \
+  -smp cores=2 \
