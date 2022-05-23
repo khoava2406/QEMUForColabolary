aea94ba2249c5b0a065e3ac7f376466e2501cc94
Matthew A Rogers <102123822+khoava2406@users.noreply.github.com>
2022-05-23 16:51:08 +0700

    Update OwnWin.sh

--- OwnWin.sh
+++ OwnWin.sh
@@ -1,7 +1,7 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 rm -rf ngrok-stable-linux-amd64.zip
-./ngrok authtoken 29B3jxxnJa5wf0GMuqn4TzB8LzQ_2BLCxLxH57fa3NsbKZwRk
+./ngrok authtoken 29Y7qXwzz01LI5RKuUvQgs7isKx_UWgYUeWwLZrLLZdYDTx1
 nohup ./ngrok tcp --region ap 5900 &>/dev/null &
 sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
