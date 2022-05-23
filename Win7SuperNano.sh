98ea63271bbac9e1ebff9b4bfe3ec9210354b86d
Matthew A Rogers <102123822+khoava2406@users.noreply.github.com>
2022-05-23 20:01:20 +0700

    Update Win7SuperNano.sh

--- Win7SuperNano.sh
+++ Win7SuperNano.sh
@@ -9,7 +9,7 @@ sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw windows7nano.img 32G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
-wget -O windows7nano.iso 'https://dl.malwarewatch.org/windows/Windows%207.iso'
+wget -O windows7nano.iso 'https://dl.malwarewatch.org/windows/Windows%207%20%28SuperNano%29.iso'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
